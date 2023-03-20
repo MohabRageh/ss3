fde3d7096f8238c171db0a7562d6e1a20306ed3c
Mohab Rageh <122291453+MohabRageh@users.noreply.github.com>
2023-03-20 12:46:34 +0200

    Update users.js

--- src/api/users.js
+++ src/api/users.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create(
     {
-        baseURL:"http://localhost:3500/"
+        baseURL:"https://github.com/MohabRageh/ss3/blob/master/src/db/data.json"
     }
 )
